2092cd43af7cfa4f0c94417f6c60bfe252950a86
joshuathurston <jthurston3@mail.csuchico.edu>
2017-03-30 21:42:49 -0700

    First problem done

---
+++ questions.js
@@ -0,0 +1,45 @@
+
+
+function answerOne()
+{
+  var answer = document.getElementById("problemOneAnswer");
+  if(isNaN(answer.value))
+  {
+    alert("Must be a number");
+  }
+  else
+  {
+    if(answer.value == 987)
+    {
+      setCookie("answer", 1, 1);
+    }
+    else
+    {
+      setCookie("answer", 0, 1);
+    }
+  }
+}
+
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+//courtesy of w3schools, from: http://www.w3schools.com/js/js_cookies.asp
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+
